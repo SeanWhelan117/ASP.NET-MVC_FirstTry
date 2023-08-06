db4c5842c464ff596a535910c02856c9786f3db5
seanwhelan117 <seanwhelan117@gmail.com>
2023-08-06 15:33:20 +0100

    Added finished code

---
+++ TrackplanInterviewSean/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrackplanInterviewSean.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
